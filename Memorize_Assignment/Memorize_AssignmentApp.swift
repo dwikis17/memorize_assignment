9a4e8908e7481a641ad8a8037f6ed41ca6bc554a
Dwiki <dwikis17@gmail.com>
2023-05-04 16:29:52 +0700

    Initial Commit

---
+++ Memorize_Assignment/Memorize_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_AssignmentApp.swift
+//  Memorize_Assignment
+//
+//  Created by Dwiki Dwiki on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
